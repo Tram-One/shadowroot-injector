20f7f43b1c0857f5de725fc89160ff67f1c7f3e1
Jesse Jurman <j.r.jurman@gmail.com>
2025-07-06 18:29:25 -0400

    update shadowroot-injector, new documentation

--- README.md
+++ README.md
@@ -5,39 +5,115 @@ _🪡 declaratively define shadowroots to repeat in HTML templates_
 ## Example
 
 ```html
-<!-- first, build a template that will be picked up by the ShadowRoot Injector -->
-<template sr-tagname="highlightable-title" sr-mode="open">
+<!-- 1. Auto-start the injector -->
+<script src="https://unpkg.com/shadowroot-injector@1" sr-autostart></script>
+
+<!-- 2. Define a generic <linked-header> -->
+<template sr-tagname="callout-alert" sr-mode="open">
   <style>
-    :host {
+    :host,
+    slot {
       display: block;
     }
+
+    div {
+      display: block;
+      border-left: solid 3px rgb(var(--callout-color));
+      background: rgba(var(--callout-color), 0.1);
+      padding: 0.5em;
+    }
+
+    slot[name='title'] {
+      font-weight: bold;
+      color: rgb(var(--callout-color));
+    }
   </style>
-  <h1>Hello <slot></slot></h1>
+
+  <div>
+    <slot name="title"></slot>
+    <slot></slot>
+  </div>
 </template>
 
-<!-- second, have instances of the component (we will automatically insert a shadow root) -->
-<highlightable-title>World</highlightable-title>
+<!-- 3. Use it anywhere -->
+<p>ShadowRoot Injector lets you repeat templates easily, no JS required!</p>
 
-<!-- third (optional), upgrade the component with dynamic behavior -->
-<script>
-  customElements.define(
-    'highlightable-title',
-    class extends HTMLElement {
-      connectedCallback() {
-        this.addEventListener('click', () => {
-          this.style.background = 'yellow';
-        });
-      }
-    },
-  );
-</script>
+<callout-alert style="--callout-color: 160, 40, 40;">
+  <span slot="title">Pro Tip!</span>
+  If you want to add more behavior, you can upgrade custom-elements into web-components any time with JavaScript!
+</callout-alert>
+
+<p>You can check out the repository on Github.</p>
+
+<callout-alert style="--callout-color: 40, 40, 160;">
+  <span slot="title">PRs Welcome!</span>
+  You can make git issues or pull requests for any issues you find.
+</callout-alert>
 ```
+
+## What is ShadowRoot Injector?
+
+ShadowRoot Injector lets you define templates for custom elements using HTML. When those elements appear in the DOM, the
+library will automatically insert the template you defined into the element. You can then, optionally, upgrade the
+element using native web-component definitions (either inline in a script tag, or imported as a separate component
+definition).
+
+### Why?
+
+Today, there isn't a native or elegant way to repeat HTML content across the document without building a javascript
+component definition. For many simple authoring use-cases, just having a template that should appear is all that web
+authors need. This library gives you an easy and elegant way to do that, without the boilerplate or complexities
+associated with building javascript class definitions.
 
 ## How to use
 
 You can include ShadowRoot Injector by using a CDN of your choice. In the script tag you can include `sr-autostart` to
-automatically kick off the mutation observers that look for and inject shadow root templates
+automatically kick off the mutation observers that look for and inject shadow root templates.
 
 ```html
 <script src="https://unpkg.com/shadowroot-injector@1" sr-autostart></script>
 ```
+
+You can also use the minified version by pointing to the minified asset
+
+```html
+<script src="https://unpkg.com/shadowroot-injector@1/shadowroot-injector.min.js" sr-autostart></script>
+```
+
+### HTML API
+
+The HTML API is completely driven by attributes on the `<template>` tag. When you include both of the following
+attributes, ShadowRoot Injector will automatically kick off and register these templates to be used for custom elements.
+
+<dl>
+  <dt><code>sr-tagname</code></dt>
+  <dd>The custom element tagname to associate with this template. When these elements appear in the DOM, we'll automatically inject a template into them. They should be a hyphenated custom element name (although they do not have to be a defined web-component).</dd>
+
+  <dt><code>sr-mode</code></dt>
+  <dd>The <a href="https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/mode">ShadowRoot mode property</a>. This must be defined as a valid value for ShadowRoot modes, either <code>open</code> or <code>closed</code>.</dd>
+</dl>
+
+You may also include any valid
+[ShadowRoot template properties](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/template#attributes),
+for example, `shadowrootdelegatesfocus`, `shadowrootclonable`, or even `shadowrootcustomelementregistry`.
+
+### JS API
+
+While not required, you can use the JavaScript API to interface directly with the ShadowRoot Injector library.
+
+<dl>
+  <dt><code>ShadowRootInjector.registerTemplateDefinition(template: HTMLTemplateElement)</code></dt>
+  <dd>This function takes in a template node (as described above in the HTML API) and registers it for use later. This is useful if you want to programmatically add templates, or want to do so manually, without a observer on the page.</dd>
+
+  <dt><code>ShadowRootInjector.injectRegisteredTemplate(node: HTMLElement)</code></dt>
+  <dd>This function takes in an HTML Element, and injects a known (registered) ShadowRoot template.</dd>
+
+  <dt><code>ShadowRootInjector.startObservers</code></dt>
+  <dd>This function starts the Mutation Observers associated with detecting new templates to register (calling <code>registerTemplateDefinition</code>), and finding custom elements to insert templates into (calling <code>injectRegisteredTemplate</code>).</dd>
+
+  <dt><code>ShadowRootInjector.stopObservers</code></dt>
+  <dd>This function stops the observers started by <code>startObservers</code>.</dd>
+</dl>
+
+> [!note] If you include `sr-autostart` in the script you used to import, an instance of the ShadowRoot Injector will
+> already be running and available to access as part of `window.shadowRootInjector`
--- shadowroot-injector.js
+++ shadowroot-injector.js
@@ -5,29 +5,41 @@ class ShadowRootInjector {
 		// map of known element tag names to shadow root template definitions
 		this.shadowRootInjectorElementMap = new Map();
 
+		/** function that takes in a mutation list and registers any templates as element definitions */
+		const checkAndRegisterTemplatesForMutationLists = (mutationList) => {
+			for (const mutation of mutationList) {
+				for (const newNode of mutation?.addedNodes || []) {
+					// if this is the element AFTER an injectable template, register that template for future elements
+					// (this will almost always be a TEXT node, even if the next actual element would be an element)
+					const previousNode = newNode.previousSibling;
+					if (previousNode && previousNode.tagName === 'TEMPLATE' && previousNode.hasAttribute('sr-mode')) {
+						this.registerTemplateDefinition(previousNode);
+					}
+				}
+			}
+		};
+
 		// Mutation Observer to register templates as known element definitions
 		this.templateDefinitionObserver = new MutationObserver((mutationList) => {
-			this.checkAndRegisterTemplatesForMutationLists(mutationList);
+			checkAndRegisterTemplatesForMutationLists(mutationList);
 		});
+
+		/** function that takes in a mutation list and inserts templates for already registered elements */
+		const checkAndInsertShadowRootsForMutationLists = (mutationList) => {
+			for (const mutation of mutationList) {
+				for (const newNode of mutation?.addedNodes || []) {
+					const newNodeTagName = newNode.tagName;
+					if (this.shadowRootInjectorElementMap.has(newNodeTagName)) {
+						this.injectRegisteredTemplate(newNode);
+					}
+				}
+			}
+		};
 
 		// Mutation Observer to look for registered elements with known template definitions
 		this.registeredElementObserver = new MutationObserver((mutationList) =>
-			this.checkAndInsertShadowRootsForMutationLists(mutationList),
+			checkAndInsertShadowRootsForMutationLists(mutationList),
 		);
-	}
-
-	/** function that attaches a shadow root template to a given node */
-	injectShadowRoot(node, template) {
-		// build the actual shadow root object in a placeholder (we will copy this later into real elements)
-		const shadowRootPlaceholder = document.createElement('div');
-
-		// using setHTMLUnsafe allows us to build the parsed version of the shadowRoot object
-		shadowRootPlaceholder.setHTMLUnsafe(`<div>${template.outerHTML}</div>`);
-		const shadowRoot = shadowRootPlaceholder.children[0].shadowRoot;
-
-		// attach a new shadow to this element using the properties of the shadowroot object that was created
-		node.attachShadow(shadowRoot);
-		node.shadowRoot.append(template.content.cloneNode(true));
 	}
 
 	/** function that registers a template to be used later with custom elements */
@@ -41,49 +53,50 @@ class ShadowRootInjector {
 		this.shadowRootInjectorElementMap.set(tagName.toUpperCase(), shadowRootTemplate);
 	}
 
-	/** function that takes in a mutation list and registers any templates as element definitions */
-	checkAndRegisterTemplatesForMutationLists(mutationList) {
-		for (const mutation of mutationList) {
-			for (const newNode of mutation?.addedNodes || []) {
-				// if this is the element AFTER an injectable template, register that template for future elements
-				// (this will almost always be a TEXT node, even if the next actual element would be an element)
-				const previousNode = newNode.previousSibling;
-				if (previousNode && previousNode.tagName === 'TEMPLATE' && previousNode.hasAttribute('sr-mode')) {
-					this.registerTemplateDefinition(previousNode);
-				}
-			}
-		}
-	}
+	/** function that attaches a registered shadow root template to a given node */
+	injectRegisteredTemplate(node) {
+		// get the template that exists for this node
+		const nodeTagName = node.tagName;
+		const template = this.shadowRootInjectorElementMap.get(nodeTagName);
 
-	/** function that takes in a mutation list and inserts templates for already registered elements */
-	checkAndInsertShadowRootsForMutationLists(mutationList) {
-		for (const mutation of mutationList) {
-			for (const newNode of mutation?.addedNodes || []) {
-				const newNodeTagName = newNode.tagName;
-				if (this.shadowRootInjectorElementMap.has(newNodeTagName)) {
-					const newNodeTemplate = this.shadowRootInjectorElementMap.get(newNodeTagName);
-					this.injectShadowRoot(newNode, newNodeTemplate);
-				}
-			}
+		// if there is no defined template, return early
+		if (!template) {
+			return;
 		}
+
+		// build the actual shadow root object in a placeholder (we will copy this later into real elements)
+		const shadowRootPlaceholder = document.createElement('div');
+
+		// using setHTMLUnsafe allows us to build the parsed version of the shadowRoot object
+		shadowRootPlaceholder.setHTMLUnsafe(`<div>${template.outerHTML}</div>`);
+		const shadowRoot = shadowRootPlaceholder.children[0].shadowRoot;
+
+		// attach a new shadow to this element using the properties of the shadowroot object that was created
+		node.attachShadow(shadowRoot);
+		node.shadowRoot.append(template.content.cloneNode(true));
 	}
 
 	/** function that starts both of the mutation observers */
-	start() {
+	startObservers() {
 		this.templateDefinitionObserver.observe(document.documentElement, { childList: true, subtree: true });
 		this.registeredElementObserver.observe(document.documentElement, { childList: true, subtree: true });
 	}
 
 	/** function to stop both of the mutation observers */
-	stop() {
+	stopObservers() {
 		this.templateDefinitionObserver.disconnect();
 		this.registeredElementObserver.disconnect();
 	}
 }
 
+// check if we are running as a module (if we are, expose the ShadowRootInjector to be imported)
+if (typeof module !== 'undefined') {
+	module.exports = ShadowRootInjector;
+}
+
 // check if the script tag has a `autostart` attribute (this indicates we should build and start the injector),
 // otherwise we'll defer to the user to do this in their own script
-if (document.currentScript.hasAttribute('sr-autostart')) {
+if (document?.currentScript.hasAttribute('sr-autostart')) {
 	window.shadowRootInjector = new ShadowRootInjector();
-	window.shadowRootInjector.start();
+	window.shadowRootInjector.startObservers();
 }
